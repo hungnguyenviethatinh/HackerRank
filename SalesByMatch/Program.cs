4425cc87e6bee4e82e2288fd4a285f56b93b29bc
Hùng Nguyễn Viết <hungnguyenviet.hatinh@gmail.com>
2023-09-25 20:48:35 +0700

    Solve Sales By Match problem.

--- SalesByMatch/Program.cs
+++ SalesByMatch/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int SockMerchant(int n, List<int> ar)
+{
+    int result = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (ar[i] == 0)
+        {
+            continue;
+        }
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (ar[i] == ar[j])
+            {
+                ar[j] = 0;
+                result++;
+
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+int n = 7;
+var ar = new List<int> { 1, 2, 1, 1, 3, 2, 1 };
+
+int result = SockMerchant(n, ar);
+
+Console.WriteLine(result);

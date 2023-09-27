c6011a2afa0544b4b34e79e818e23283518d0620
Hung Nguyen Viet (SM) <hung.nguyen.viet@dmchosting.ca>
2023-09-27 15:34:16 +0700

    Solve Repeated String problem.

--- RepeatedString/Program.cs
+++ RepeatedString/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static long RepeatedString(string s, long n)
+{
+    long result = 0;
+    long restResult = 0;
+    int rest = (int)(n % s.Length);
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == 'a')
+        {
+            result++;
+
+            if (i < rest)
+            {
+                restResult++;
+            }
+        }
+    }
+
+    result *= n / s.Length;
+
+    return result + restResult;
+}
+
+string s = "aba";
+long n = 10;
+
+long result = RepeatedString(s, n);
+
+Console.WriteLine(result);

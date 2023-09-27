c27898a4fb41b7ae764bf050e86191aecdedf43e
Hung Nguyen Viet (SM) <hung.nguyen.viet@dmchosting.ca>
2023-09-27 14:54:06 +0700

    Solve Jumping On The Cloulds problem.

--- JumpingOnTheClouds/Program.cs
+++ JumpingOnTheClouds/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int JumpingOnTheClouds(List<int> c)
+{
+    int result = 0;
+
+    int current = 0;
+
+    for (int i = 0; i < c.Count - 1; i++)
+    {
+        if (i != current)
+        {
+            continue;
+        }
+        
+        if (i + 2 < c.Count && c[i + 2] == 0)
+        {
+            current = i + 2;
+            result++;
+        }
+        else if (c[i + 1] == 0)
+        {
+            current = i + 1;
+            result++;
+        }
+    }
+
+    return result;
+}
+
+var c = new List<int> { 0, 0, 0, 1, 0, 0 };
+
+int result = JumpingOnTheClouds(c);
+
+Console.WriteLine(result);
